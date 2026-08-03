60f59a953055a65d2def45f2de6fa4828599f9eb
Berzki <mhdrizky32@gmail.com>
2026-08-03 15:43:21 +0700

    Initial Commit

---
+++ POC-Shortcuts/POC_ShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_ShortcutsApp.swift
+//  POC-Shortcuts
+//
+//  Created by Muhammad Rizki on 03/08/26.
+//
+
+import SwiftUI
+
+@main
+struct POC_ShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
